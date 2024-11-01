6179709cc5e3b23ff1d3881dcc0dc36980de6268
Emir <ebasic@gmail.com>
2024-11-01 23:54:45 +0100

    add 03-functions

---
+++ 03-functions/helloFunc2.py
@@ -0,0 +1,9 @@
+def hello(name):
+    print('Hello ' + name)
+
+
+hello('Alice')
+hello('Bob')
+
+# Argument = The value passed in the function call (Alice, Bob)
+# Parameter = The variable inside the function (name)
