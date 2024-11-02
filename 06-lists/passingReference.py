f8f7d3549aedb51ad31f66ebb063bac1ae3e6b30
Emir <ebasic@gmail.com>
2024-11-02 19:43:18 +0100

    add lists

---
+++ 06-lists/passingReference.py
@@ -0,0 +1,15 @@
+# Passing Reference
+
+# When function is called, the value of arguments are copied to the parameter variables.
+# For the lists, a copy of the reference is used for the parameter
+# The append(), extend(), remove(), sort(), reverse(), and other list methods
+# modifiy their lists in place.
+
+
+def eggs(someParameter):
+    someParameter.append('Hello')
+
+
+spam = [1, 2, 3]
+eggs(spam)
+print(spam)
